9796847ed0b1b012b6ba19cc88cc15b42794e3ab
Systemowner13 <Khaled_ka@hotmail.com>
2026-05-01 16:03:54 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = "poke-registry-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll([
+      "/",
+      "/index.html",
+      "/manifest.json"
+    ]))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
